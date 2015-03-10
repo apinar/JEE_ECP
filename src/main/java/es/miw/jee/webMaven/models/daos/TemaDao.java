52df9956e09a9c98667b848365baa2044bf407a2
Alvaro <Alvaro@apinar>
2015-03-10 20:14:18 +0100

    Creadas interfaces TemaDao y VotoDao

---
+++ src/main/java/es/miw/jee/webMaven/models/daos/TemaDao.java
@@ -0,0 +1,7 @@
+package es.miw.jee.webMaven.models.daos;
+
+import es.miw.jee.webMaven.models.entities.Tema;
+
+public interface TemaDao extends GenericDao<Tema, Integer>{
+
+}
---
+++ src/main/java/es/miw/jee/webMaven/models/daos/VotoDao.java
@@ -0,0 +1,7 @@
+package es.miw.jee.webMaven.models.daos;
+
+import es.miw.jee.webMaven.models.entities.Voto;
+
+public interface VotoDao extends GenericDao<Voto, Integer>{
+
+}
