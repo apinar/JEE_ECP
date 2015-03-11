1e518564943d9880f61217b6116cb8e6c5758a2d
Alvaro <Alvaro@apinar>
2015-03-11 22:05:06 +0100

    Refacor de los tests. FIN por hoy.

--- src/test/java/es/miw/jee/webMaven/models/daos/jpa/TemaDaoJpaTest.java
+++ src/test/java/es/miw/jee/webMaven/models/daos/jpa/TemaDaoJpaTest.java
@@ -2,6 +2,7 @@ package es.miw.jee.webMaven.models.daos.jpa;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
@@ -11,6 +12,8 @@ import org.junit.Test;
 import es.miw.jee.webMaven.models.daos.DaoFactory;
 import es.miw.jee.webMaven.models.daos.TemaDao;
 import es.miw.jee.webMaven.models.entities.Tema;
+import es.miw.jee.webMaven.models.entities.Voto;
+import es.miw.jee.webMaven.models.utils.Estudios;
 
 public class TemaDaoJpaTest {
 	
@@ -32,41 +35,50 @@ public class TemaDaoJpaTest {
 		tema = new Tema();
 		tema.setNombre("Futbol");
 		tema.setPregunta("Valora la importancia de Iniesta en la historia del fútbol español");
+		Voto voto = new Voto();
+		voto.setValoracion(7);
+		voto.setNivelEstudios(Estudios.PRIMARIA);
+		voto.setIp("138.100.123.105");
+		List<Voto> votos = new ArrayList<Voto>(1);
+		votos.add(voto);
+		tema.setVotos(votos);
+		
 		temaDaoJpa.create(tema);
     }
 	
-		
+			
 	@Test
 	public void testCreate() {	
+		int index = temaDaoJpa.findAll().get(0).getId();
 		List<Tema> temas = temaDaoJpa.findAll();
 		assertEquals(temas.isEmpty(), false);
+		temaDaoJpa.deleteById(index);
 	}
 
-	
+
 	@Test
 	public void testReadNombre(){
-		assertEquals(temaDaoJpa.read(1).getNombre(), tema.getNombre());	
+		int index = temaDaoJpa.findAll().get(0).getId();
+		assertEquals(temaDaoJpa.read(index).getNombre(), tema.getNombre());
+		temaDaoJpa.deleteById(index);
 	}
 	
-	
+			
 	@Test
 	public void testReadPregunta(){
-		assertEquals(temaDaoJpa.read(1).getPregunta(), tema.getPregunta());	
+		int index = temaDaoJpa.findAll().get(0).getId();
+		assertEquals(temaDaoJpa.read(index).getPregunta(), tema.getPregunta());
+		temaDaoJpa.deleteById(index);
 	}
 	
-	
-	@Test
-	public void testReadVotos(){
-		assertEquals(temaDaoJpa.read(1).getVotos(), tema.getVotos());	
-	}
-	
-	
+		
 	@Test
 	public void testUpdateNombre(){
 		tema.setNombre("Tecnología");
 		temaDaoJpa.update(tema);
-		int index = temaDaoJpa.findAll().size();
+		int index = temaDaoJpa.findAll().get(0).getId();
 		assertEquals( temaDaoJpa.read(index).getNombre(), tema.getNombre());
+		temaDaoJpa.deleteById(index);
 	}
 	
 	
@@ -74,22 +86,26 @@ public class TemaDaoJpaTest {
 	public void testUpdatePregunta(){
 		tema.setPregunta("Valora la utilidad del Apple Watch.");
 		temaDaoJpa.update(tema);
-		int index = temaDaoJpa.findAll().size();
+		int index = temaDaoJpa.findAll().get(0).getId();
 		assertEquals( temaDaoJpa.read(index).getPregunta(), tema.getPregunta());
+		temaDaoJpa.deleteById(index);
 	}
 	
 		
 	@Test
 	public void testDeleteById(){
-		int index = temaDaoJpa.findAll().size();
+		int index = temaDaoJpa.findAll().get(0).getId();
 		temaDaoJpa.deleteById(index);
 		assertNull(temaDaoJpa.read(index));
+		
 	}
 	
 	
 	@Test
 	public void testFindAll(){
+		int index = temaDaoJpa.findAll().get(0).getId();
 		assertNotNull(temaDaoJpa.findAll().size());
-		
+		temaDaoJpa.deleteById(index);
 	}
+	
 }
--- src/test/java/es/miw/jee/webMaven/models/daos/jpa/VotoDaoJpaTest.java
+++ src/test/java/es/miw/jee/webMaven/models/daos/jpa/VotoDaoJpaTest.java
@@ -1,5 +1,107 @@
 package es.miw.jee.webMaven.models.daos.jpa;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import es.miw.jee.webMaven.models.daos.DaoFactory;
+import es.miw.jee.webMaven.models.daos.VotoDao;
+import es.miw.jee.webMaven.models.entities.Voto;
+import es.miw.jee.webMaven.models.utils.Estudios;
+
+
 public class VotoDaoJpaTest {
  
+	public VotoDao votoDaoJpa;
+	private Voto voto;
+	
+	@BeforeClass
+	public static void beforeClass() {
+		DaoFactory.setFactory(new DaoJpaFactory());
+		DaoJpaFactory.prepareFactoryWithDropAndCreateTables();
+	}
+
+	@Before
+	public void ini() {
+		votoDaoJpa = DaoFactory.getFactory().getVotoDao();
+		voto = new Voto();
+		voto.setValoracion(8);
+		voto.setNivelEstudios(Estudios.SUPERIOR);
+		voto.setIp("138.100.123.101");
+		votoDaoJpa.create(voto);
+    }
+
+	
+	@Test
+	public void testCreate() {	
+		assertEquals(votoDaoJpa.findAll().isEmpty() , false);
+	}
+	
+	
+	@Test
+	public void testReadValoracion() {
+		int index = votoDaoJpa.findAll().size();
+		assertEquals(votoDaoJpa.read(index).getValoracion(), voto.getValoracion());
+	}
+	
+	
+	@Test
+	public void testReadEstudios() {
+		int index = votoDaoJpa.findAll().size();
+		assertEquals(votoDaoJpa.read(index).getNivelEstudios(), voto.getNivelEstudios());
+	}
+	
+	
+	@Test
+	public void testReadIp() {
+		int index = votoDaoJpa.findAll().size();
+		assertEquals(votoDaoJpa.read(index).getIp(), voto.getIp());
+	}
+	
+	@Test
+	public void testUpdateValoracion(){
+		voto.setValoracion(9);
+		votoDaoJpa.update(voto);
+		int index = votoDaoJpa.findAll().size();
+		assertEquals( votoDaoJpa.read(index).getValoracion(), voto.getValoracion());
+	}
+	
+	
+	@Test
+	public void testUpdateEstudios(){
+		voto.setNivelEstudios(Estudios.SECUNDARIA);
+		votoDaoJpa.update(voto);
+		int index = votoDaoJpa.findAll().size();
+		assertEquals( votoDaoJpa.read(index).getNivelEstudios(), voto.getNivelEstudios());
+	}
+	
+	
+	@Test
+	public void testUpdateIp(){
+		voto.setIp("138.100.123.103");
+		votoDaoJpa.update(voto);
+		int index = votoDaoJpa.findAll().size();
+		assertEquals( votoDaoJpa.read(index).getIp(), voto.getIp());
+	}
+	
+	
+	@Test
+	public void testDeleteById(){
+		int index = votoDaoJpa.findAll().size();
+		votoDaoJpa.deleteById(index);
+		assertNull(votoDaoJpa.read(index));
+		votoDaoJpa.create(voto);
+	}
+	
+	
+	@Test
+	public void testFindAll(){
+		assertNotNull(votoDaoJpa.findAll().size());
+		
+	}
+	
 }
