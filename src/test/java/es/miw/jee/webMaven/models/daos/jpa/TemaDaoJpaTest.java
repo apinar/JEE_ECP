2a433a45b485c5bae09fd330c0874256f35713cf
Alvaro <Alvaro@apinar>
2015-03-11 20:19:49 +0100

    Refactor de TemaDaoJpatest. Incluidos todos los tests de la clase.

--- src/test/java/es/miw/jee/webMaven/models/daos/jpa/TemaDaoJpaTest.java
+++ src/test/java/es/miw/jee/webMaven/models/daos/jpa/TemaDaoJpaTest.java
@@ -15,27 +15,81 @@ import es.miw.jee.webMaven.models.entities.Tema;
 public class TemaDaoJpaTest {
 	
 	public TemaDao temaDaoJpa;
-
+	private Tema tema;
+		
+	
+		
 	@BeforeClass
 	public static void beforeClass() {
 		DaoFactory.setFactory(new DaoJpaFactory());
-	}
-
-	@Before
-	public void before() {
 		DaoJpaFactory.prepareFactoryWithDropAndCreateTables();
-		temaDaoJpa = DaoFactory.getFactory().getTemaDao();
 	}
 
-	@Test
-	public void testCreate() {
-		Tema tema = new Tema();
+		
+	@Before
+	public void ini() {
+		temaDaoJpa = DaoFactory.getFactory().getTemaDao();
+		tema = new Tema();
 		tema.setNombre("Futbol");
-		tema.setPregunta("¿Es Iniesta el mejor jugador español de la historia?");
+		tema.setPregunta("Valora la importancia de Iniesta en la historia del fútbol español");
 		temaDaoJpa.create(tema);
+    }
+	
+		
+	@Test
+	public void testCreate() {	
 		List<Tema> temas = temaDaoJpa.findAll();
-		assertEquals("OK", temas.size(), 1);
+		assertEquals(temas.isEmpty(), false);
+	}
+
+	
+	@Test
+	public void testReadNombre(){
+		assertEquals(temaDaoJpa.read(1).getNombre(), tema.getNombre());	
 	}
 	
 	
+	@Test
+	public void testReadPregunta(){
+		assertEquals(temaDaoJpa.read(1).getPregunta(), tema.getPregunta());	
+	}
+	
+	
+	@Test
+	public void testReadVotos(){
+		assertEquals(temaDaoJpa.read(1).getVotos(), tema.getVotos());	
+	}
+	
+	
+	@Test
+	public void testUpdateNombre(){
+		tema.setNombre("Tecnología");
+		temaDaoJpa.update(tema);
+		int index = temaDaoJpa.findAll().size();
+		assertEquals( temaDaoJpa.read(index).getNombre(), tema.getNombre());
+	}
+	
+	
+	@Test
+	public void testUpdatePregunta(){
+		tema.setPregunta("Valora la utilidad del Apple Watch.");
+		temaDaoJpa.update(tema);
+		int index = temaDaoJpa.findAll().size();
+		assertEquals( temaDaoJpa.read(index).getPregunta(), tema.getPregunta());
+	}
+	
+		
+	@Test
+	public void testDeleteById(){
+		int index = temaDaoJpa.findAll().size();
+		temaDaoJpa.deleteById(index);
+		assertNull(temaDaoJpa.read(index));
+	}
+	
+	
+	@Test
+	public void testFindAll(){
+		assertNotNull(temaDaoJpa.findAll().size());
+		
+	}
 }
