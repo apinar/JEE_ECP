25bdb601344537e1ef5337bcb90b036dd1e0354e
Alvaro <Alvaro@apinar>
2015-03-24 22:02:12 +0100

    Creado el Dispatcher a partir del ejemplo de clase. CON ERRORES.

---
+++ src/main/java/es/miw/jee/webMaven/views/web/beans/Dispatcher.java
@@ -0,0 +1,63 @@
+package es.miw.jee.webMaven.views.web.beans;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/jsp/*")
+public class Dispatcher extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static String PATH_ROOT_VIEW = "/personaViewsV1/";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String action = request.getPathInfo().substring(1);
+
+        String view;
+        switch (action) {
+        case "persona":
+            
+        	view = action;
+            break;
+        case "rol":
+        	view = action;
+        default:
+            view = "home";
+        }
+
+        this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
+                .forward(request, response);
+    }
+	
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getPathInfo().substring(1);
+        String view = "home";
+        switch (action) {
+        case "persona":
+
+            view = personaView.process();
+            break;
+        case "rol":
+
+            view = rolView.process();
+            break;
+        }
+
+        this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
+                .forward(request, response);
+    }
+    
+	
+}
