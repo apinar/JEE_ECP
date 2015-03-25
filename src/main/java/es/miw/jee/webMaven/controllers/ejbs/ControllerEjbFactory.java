9f6eb469cb9ae2d7a663cb729aacb8d554f05cac
Alvaro <Alvaro@apinar>
2015-03-25 19:58:33 +0100

    Creado ControllerEjbFactory vacío.

---
+++ src/main/java/es/miw/jee/webMaven/controllers/ejbs/ControllerEjbFactory.java
@@ -0,0 +1,35 @@
+package es.miw.jee.webMaven.controllers.ejbs;
+
+import es.miw.jee.webMaven.controllers.ControllerFactory;
+import es.miw.jee.webMaven.controllers.EliminarTemaController;
+import es.miw.jee.webMaven.controllers.IncorporarTemaController;
+import es.miw.jee.webMaven.controllers.VerVotacionesController;
+import es.miw.jee.webMaven.controllers.VotarController;
+
+public class ControllerEjbFactory extends ControllerFactory{
+
+	@Override
+	public EliminarTemaController getEliminarTemaController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IncorporarTemaController getIncorporarTemaController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VerVotacionesController getVerVotacionesController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VotarController getVotarController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
