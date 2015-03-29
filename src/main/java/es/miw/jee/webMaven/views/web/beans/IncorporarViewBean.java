e8974b87e21ed77433b2a2fc608555d15eedbae4
Alvaro <Alvaro@apinar>
2015-03-29 17:56:59 +0200

    Implementado IncorporarViewBean

--- src/main/java/es/miw/jee/webMaven/views/web/beans/IncorporarViewBean.java
+++ src/main/java/es/miw/jee/webMaven/views/web/beans/IncorporarViewBean.java
@@ -1,5 +1,38 @@
 package es.miw.jee.webMaven.views.web.beans;
 
+import javax.faces.bean.ManagedBean;
+
+import es.miw.jee.webMaven.controllers.IncorporarTemaController;
+import es.miw.jee.webMaven.models.entities.Tema;
+
+
+@ManagedBean
 public class IncorporarViewBean extends ViewBean {
+	
+	private String nombre;
+	private String pregunta;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPregunta() {
+		return pregunta;
+	}
+	public void setPregunta(String pregunta) {
+		this.pregunta = pregunta;
+	}
+	
+	
+	public String process() {
+		Tema tema = new Tema();
+		tema.setNombre(nombre);
+		tema.setPregunta(pregunta);
+		IncorporarTemaController incorporarTemaController = this.getControllerFactory().getIncorporarTemaController(); 
+		incorporarTemaController.incorporarTema(tema);
+		return null;
+	}
 
 }
