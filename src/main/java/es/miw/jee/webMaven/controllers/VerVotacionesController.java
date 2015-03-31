2ab91608eb02345064a7b06c918533c92f498a30
Alvaro <Alvaro@apinar>
2015-04-01 00:48:18 +0200

    Segunda aproximación. FIN por hoy.

--- src/main/java/es/miw/jee/webMaven/controllers/VerVotacionesController.java
+++ src/main/java/es/miw/jee/webMaven/controllers/VerVotacionesController.java
@@ -3,9 +3,11 @@ package es.miw.jee.webMaven.controllers;
 import java.util.List;
 
 import es.miw.jee.webMaven.models.entities.Tema;
+import es.miw.jee.webMaven.models.utils.Estudios;
 
 public interface VerVotacionesController {
 
 	List<Tema> listarTemas();
 	List<Integer> verVotos();
+	Double mediaVotosPorEstudioYTema(Estudios estudio, Tema tema);
 }
--- src/main/java/es/miw/jee/webMaven/controllers/ejbs/VerVotacionesControllerEjb.java
+++ src/main/java/es/miw/jee/webMaven/controllers/ejbs/VerVotacionesControllerEjb.java
@@ -7,11 +7,15 @@ import es.miw.jee.webMaven.controllers.VerVotacionesController;
 import es.miw.jee.webMaven.models.daos.TemaDao;
 import es.miw.jee.webMaven.models.daos.jpa.DaoJpaFactory;
 import es.miw.jee.webMaven.models.entities.Tema;
+import es.miw.jee.webMaven.models.entities.Voto;
+import es.miw.jee.webMaven.models.utils.Estudios;
 
 public class VerVotacionesControllerEjb implements VerVotacionesController {
 
 	private List<Tema> temas;
 	private List<Integer> votos;
+	private List<Estudios> estudios;
+	
 
 	
 	public List<Tema> getTemas() {
@@ -30,11 +34,21 @@ public class VerVotacionesControllerEjb implements VerVotacionesController {
 		this.votos = votos;
 	}
 
+
+	public List<Estudios> getEstudios() {
+		return estudios;
+	}
+
+	public void setEstudios(List<Estudios> estudios) {
+		this.estudios = estudios;
+	}
+	
 	@Override
 	public List<Tema> listarTemas() {
 		TemaDao temaDaoJpa = new DaoJpaFactory().getTemaDao();
 		return temaDaoJpa.findAll();
 	}
+	
 
 	@Override
 	public List<Integer> verVotos() {
@@ -47,6 +61,23 @@ public class VerVotacionesControllerEjb implements VerVotacionesController {
 		return votos;
 	}
 
-	//Falta votacion media
+	@Override
+	public Double mediaVotosPorEstudioYTema(Estudios estudio, Tema tema) {
+		List<Voto> votos = tema.getVotos();
+		List<Integer> votosPorEstudio = new ArrayList<Integer>();
+		for (Voto voto : votos){
+			if (voto.getNivelEstudios().equals(estudio.toString())){
+				votosPorEstudio.add(voto.getValoracion());
+			}
+		
+		}	
+		Double count=0.0; 
+		for (Integer valoracion : votosPorEstudio){
+			count+=valoracion;
+		}
+		return count/votosPorEstudio.size();
+	}
+
 	
+		
 }
